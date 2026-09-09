07eb42e874534931f7320e4dada305ba673d34e8
Krishna0087 <krishnahon07@gmail.com>
2026-09-09 23:12:17 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAn7VQ39SFDxcRGuav2uM8hkPj5Z-5rXCE",
+  authDomain: "jay-bhavani-medical.firebaseapp.com",
+  projectId: "jay-bhavani-medical",
+  storageBucket: "jay-bhavani-medical.firebasestorage.app",
+  messagingSenderId: "946234447721",
+  appId: "1:946234447721:web:e26f61b98c8b6f27888ff8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
